306665a5586dfdf85941cfd9d5a9821d482801e4
Jeff Rund <jeff.rund@taluserp.com>
2020-07-15 06:33:19 -0400

    helpdesk updated

--- talus_helpdesk_updates/__manifest__.py
+++ talus_helpdesk_updates/__manifest__.py
@@ -3,15 +3,14 @@
     'name': "talus_helpdesk_updates",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Helpdesk Updates""",
 
     'description': """
-        Long description of module's purpose
+        Helpdesk updates, changing description to html, adding Internal notes tab.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Talus ERP",
+    'website': "http://www.taluserp.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
